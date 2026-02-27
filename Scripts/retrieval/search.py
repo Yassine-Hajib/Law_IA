6bf803b9e813f2dbb41cd10fb857c5db400fbc25
YSFroot <bnmussef@gmail.com>
2026-02-27 22:37:13 +0000

    semantic search between UserQuery and ChromaDB

--- Scripts/retrieval/search.py
+++ Scripts/retrieval/search.py
@@ -12,3 +12,40 @@ model = SentenceTransformer("BAAI/bge-small-en")
 client = chromadb.PersistentClient(path="../embeeding/chroma_db")
 collection = client.get_collection("law_articles")
 
+# ---------------------------
+# User query
+# ---------------------------
+query = "licenciement et droits du salarié après rupture du contrat"
+query = query.strip().lower()
+
+# ---------------------------
+# Convert question to embedding
+# ---------------------------
+query_embedding = model.encode(query).tolist()
+
+# ---------------------------
+# Retrieve Top 5 documents
+# ---------------------------
+results = collection.query(
+    query_embeddings=[query_embedding],
+    n_results=8
+)
+
+documents = results["documents"][0]
+
+print("\nTop documents from Chroma:\n")
+for doc in documents:
+    print("-", doc[:150], "...\n")
+
+# ---------------------------
+# Re-ranking with Cross-Encoder
+# ---------------------------
+cross_encoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+pairs = [(query, doc) for doc in documents]
+scores = cross_encoder.predict(pairs)
+
+ranked = sorted(zip(documents, scores), key=lambda x: x[1], reverse=True)
+
+print("\nTop article after re-ranking:\n")
+print(ranked[0][0])
