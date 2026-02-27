f9cf3001f90dc5342d4969e25a29760a34ddd7de
YSFroot <bnmussef@gmail.com>
2026-02-27 22:32:33 +0000

    load embedding & ChromaDB code

--- Scripts/retrieval/search.py
+++ Scripts/retrieval/search.py
@@ -1,22 +1,14 @@
 import chromadb
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, CrossEncoder
 
-# Load model
+# ---------------------------
+# Load embedding model
+# ---------------------------
 model = SentenceTransformer("BAAI/bge-small-en")
 
+# ---------------------------
 # Load ChromaDB
+# ---------------------------
 client = chromadb.PersistentClient(path="../embeeding/chroma_db")
 collection = client.get_collection("law_articles")
 
-query = "j'ai perdu mon travail , qu'est ce que je dois faire ? "
-
-# Convert question to embedding
-query_embedding = model.encode(query).tolist()
-
-# Search
-results = collection.query(
-    query_embeddings=[query_embedding],
-    n_results=3
-)
-
-print(results["documents"])
